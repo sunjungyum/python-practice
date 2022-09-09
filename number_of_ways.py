702ec891f4c418b6c354b3246368a6a538f6bebe
Sun-Jung Yum <sunjungyum@college.harvard.edu>
2022-09-09 16:01:10 -0400

    Fix linting
    Used pylint to conform with Python formatting.

--- number_of_ways.py
+++ number_of_ways.py
@@ -1,22 +1,23 @@
+"""Compute the number of ways to get between two points."""
 from tqdm import tqdm
 
 
-def numberOfWays(startPos: int, endPos: int, k: int) -> int:
+def number_of_ways(start_pos: int, end_pos: int, k: int) -> int:
     """
     Solving Leetcode Problem.
     https://leetcode.com/problems/number-of-ways-to-reach-a-position-after-exactly-k-steps/
-    
-    Given two positive integers startPos and endPos
-    Initially, you are standing at position startPos on an infinite
+   
+    Given two positive integers start_pos and end_pos
+    Initially, you are standing at position start_pos on an infinite
     number line. With one step, you can move either one position to the left,
     or one position to the right.
     
     Given a positive integer k, return the number of different ways to
-    reach the position endPos starting from startPos, such that you
+    reach the position end_pos starting from start_pos, such that you
     perform exactly k steps.
     """
     # start with path of length 1
-    paths = [[startPos]]
+    paths = [[start_pos]]
 
     # loop k times
     for i in tqdm(range(k)):
@@ -25,7 +26,7 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
             last_position = new_path[-1]
 
             # exist fast if not going to make to end
-            if endPos - last_position > (k - i):
+            if end_pos - last_position > (k - i):
                 continue
             # path that goes to the left
             new_path_left = new_path + [last_position - 1]
@@ -39,7 +40,7 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
 
     num_ways = 0
     for path in paths:
-        if path[-1] == endPos:
+        if path[-1] == end_pos:
             num_ways += 1
     return num_ways
 
@@ -48,7 +49,7 @@ def test_number_of_ways():
     """
     Example 1:
 
-    Input: startPos = 1, endPos = 2, k = 3
+    Input: _p = 1, end_pos = 2, k = 3
     Output: 3
     Explanation: We can reach position 2 from 1 in exactly 3 steps
     in three ways:
@@ -57,7 +58,7 @@ def test_number_of_ways():
     - 1 -> 0 -> 1 -> 2.
     It can be proven that no other way is possible, so we return 3.
     """
-    print(numberOfWays(1, 2, 3))
+    print(number_of_ways(1, 2, 3))
 
 if __name__ == "__main__":
     test_number_of_ways()
